e4c09f5229718075c39cfef90dfd731a1b0c698c
jinwn <jinwn89@gmail.com>
2025-06-10 13:42:42 +0800

    读取文件目录处理器

--- lib/window/ipcEvents.ts
+++ lib/window/ipcEvents.ts
@@ -1,5 +1,6 @@
-import { type BrowserWindow, ipcMain, shell } from 'electron'
-import os from 'os'
+import { readDir } from "@/lib/main/dirs"
+import { type BrowserWindow, ipcMain, session, shell } from "electron"
+import os from "os"
 
 const handleIPC = (channel: string, handler: (...args: any[]) => void) => {
   ipcMain.handle(channel, handler)
@@ -10,7 +11,7 @@ export const registerWindowIPC = (mainWindow: BrowserWindow) => {
   mainWindow.setMenuBarVisibility(false)
 
   // Register window IPC
-  handleIPC('init-window', () => {
+  handleIPC("init-window", () => {
     const { width, height } = mainWindow.getBounds()
     const minimizable = mainWindow.isMinimizable()
     const maximizable = mainWindow.isMaximizable()
@@ -19,12 +20,12 @@ export const registerWindowIPC = (mainWindow: BrowserWindow) => {
     return { width, height, minimizable, maximizable, platform }
   })
 
-  handleIPC('is-window-minimizable', () => mainWindow.isMinimizable())
-  handleIPC('is-window-maximizable', () => mainWindow.isMaximizable())
-  handleIPC('window-minimize', () => mainWindow.minimize())
-  handleIPC('window-maximize', () => mainWindow.maximize())
-  handleIPC('window-close', () => mainWindow.close())
-  handleIPC('window-maximize-toggle', () => {
+  handleIPC("is-window-minimizable", () => mainWindow.isMinimizable())
+  handleIPC("is-window-maximizable", () => mainWindow.isMaximizable())
+  handleIPC("window-minimize", () => mainWindow.minimize())
+  handleIPC("window-maximize", () => mainWindow.maximize())
+  handleIPC("window-close", () => mainWindow.close())
+  handleIPC("window-maximize-toggle", () => {
     if (mainWindow.isMaximized()) {
       mainWindow.unmaximize()
     } else {
@@ -33,19 +34,43 @@ export const registerWindowIPC = (mainWindow: BrowserWindow) => {
   })
 
   const webContents = mainWindow.webContents
-  handleIPC('web-undo', () => webContents.undo())
-  handleIPC('web-redo', () => webContents.redo())
-  handleIPC('web-cut', () => webContents.cut())
-  handleIPC('web-copy', () => webContents.copy())
-  handleIPC('web-paste', () => webContents.paste())
-  handleIPC('web-delete', () => webContents.delete())
-  handleIPC('web-select-all', () => webContents.selectAll())
-  handleIPC('web-reload', () => webContents.reload())
-  handleIPC('web-force-reload', () => webContents.reloadIgnoringCache())
-  handleIPC('web-toggle-devtools', () => webContents.toggleDevTools())
-  handleIPC('web-actual-size', () => webContents.setZoomLevel(0))
-  handleIPC('web-zoom-in', () => webContents.setZoomLevel(webContents.zoomLevel + 0.5))
-  handleIPC('web-zoom-out', () => webContents.setZoomLevel(webContents.zoomLevel - 0.5))
-  handleIPC('web-toggle-fullscreen', () => mainWindow.setFullScreen(!mainWindow.fullScreen))
-  handleIPC('web-open-url', (_e, url) => shell.openExternal(url))
+  handleIPC("web-undo", () => webContents.undo())
+  handleIPC("web-redo", () => webContents.redo())
+  handleIPC("web-cut", () => webContents.cut())
+  handleIPC("web-copy", () => webContents.copy())
+  handleIPC("web-paste", () => webContents.paste())
+  handleIPC("web-delete", () => webContents.delete())
+  handleIPC("web-select-all", () => webContents.selectAll())
+  handleIPC("web-reload", () => webContents.reload())
+  handleIPC("web-force-reload", () => webContents.reloadIgnoringCache())
+  handleIPC("web-toggle-devtools", () => webContents.toggleDevTools())
+  handleIPC("web-actual-size", () => webContents.setZoomLevel(0))
+  handleIPC("web-zoom-in", () =>
+    webContents.setZoomLevel(webContents.zoomLevel + 0.5),
+  )
+  handleIPC("web-zoom-out", () =>
+    webContents.setZoomLevel(webContents.zoomLevel - 0.5),
+  )
+  handleIPC("web-toggle-fullscreen", () =>
+    mainWindow.setFullScreen(!mainWindow.fullScreen),
+  )
+  handleIPC("web-open-url", (_e, url) => shell.openExternal(url))
+
+  handleIPC("read-dirs", async (_e, { path, showHidden, dirOnly }) => {
+    const home = process.env.HOME!
+
+    let items = await readDir({
+      path: path || home,
+      showHidden,
+      dirOnly,
+    })
+    return items
+  })
+  handleIPC("main-dir", async () => {
+    const home = process.env.HOME!
+    const cookies = await session.defaultSession.cookies.get({
+      name: "main-dir",
+    })
+    return cookies.length ? cookies[0] : home
+  })
 }
